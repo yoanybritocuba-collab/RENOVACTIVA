34b1618b1498be56fef4e5f5f89eae124b701677
Yoany Brito <yoanybritocuba@gmail.com>
2026-09-12 21:56:25 +0200

    Restaurar dashboard admin + limpiar archivos temporales

--- app/admin/test/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function TestPage() {
-  return (
-    <div className="min-h-screen bg-red-500 flex items-center justify-center text-white text-4xl">
-      ¡TEST FUNCIONA!
-    </div>
-  )
-}
