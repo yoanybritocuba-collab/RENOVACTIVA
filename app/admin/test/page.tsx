9789d71743b665f55ff5a0b9bca4e124e9d84c4e
Yoany Brito <yoanybritocuba@gmail.com>
2026-09-12 21:43:42 +0200

    Añadir página de test

---
+++ app/admin/test/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function TestPage() {
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center text-white text-4xl">
+      ¡TEST FUNCIONA!
+    </div>
+  )
+}
